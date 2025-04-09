88bde1483f57aec63572d18ca76f4658680b5bbf
Jeremi Nowak <s197611@student.pg.edu.pl>
2025-04-09 20:24:51 +0200

    Zrobienie ładnego navi

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 window.addEventListener('scroll', function () {
   const nav = document.getElementById('top-nav');
   
-  if(window.scrollY >= 100){
-	  nav.classList.add('out');
-  }
-  else{
-	  nav.classList.remove('out');
-  }
-  if (window.scrollY >= 650) {
+  if (window.scrollY >= 20) {
     nav.classList.add('fixed');
-	nav.classList.remove('out');
   } else {
     nav.classList.remove('fixed');
   }
