45860e6095cf0ad72cc617ed6c8063f747434a39
Jeremi Nowak <s197611@student.pg.edu.pl>
2025-12-21 16:59:38 +0100

    Małe poprawki w adresacji

--- script.js
+++ script.js
@@ -25,16 +25,77 @@ const topNav = document.getElementById("top-nav");
 
 document.getElementById("burgerBtn").onclick = function(){
   window.scrollTo({top: 0, behavior: 'smooth'});
-  mobNav.classList.remove("ukryty");
+  mobNav.classList.add("active");
   topNav.classList.add("ukryty");
   document.body.classList.add("noscroll");
 };
 
 document.getElementById("closeBtn").onclick = function(){
-  mobNav.classList.add("ukryty");
+  mobNav.classList.remove("active");
   topNav.classList.remove("ukryty");
   document.body.classList.remove("noscroll");
 };
+
+document.getElementById("about_menu").onclick = function(){
+  mobNav.classList.remove("active");
+  topNav.classList.remove("ukryty");
+  document.body.classList.remove("noscroll");
+
+  const sekcjaAbout = document.getElementById("about"); 
+  sekcjaAbout.scrollIntoView({behavior: 'smooth'});
+};
+
+document.getElementById("wina_menu").onclick = function(){
+  mobNav.classList.remove("active");
+  topNav.classList.remove("ukryty");
+  document.body.classList.remove("noscroll");
+
+  const sekcjaWina = document.getElementById("wina"); 
+  sekcjaWina.scrollIntoView({behavior: 'smooth'});
+};
+
+document.getElementById("oferta_menu").onclick = function(){
+  mobNav.classList.remove("active");
+  topNav.classList.remove("ukryty");
+  document.body.classList.remove("noscroll");
+
+  const sekcjaOferta = document.getElementById("oferta"); 
+  sekcjaOferta.scrollIntoView({behavior: 'smooth'});
+};
+
+document.getElementById("kontakt_menu").onclick = function(){
+  mobNav.classList.remove("active");
+  topNav.classList.remove("ukryty");
+  document.body.classList.remove("noscroll");
+
+  const sekcjaKontakt = document.getElementById("kontakt"); 
+  sekcjaKontakt.scrollIntoView({behavior: 'smooth'});
+};
+
+
+document.getElementById("about_menu1").onclick = function(){
+  const sekcjaAbout = document.getElementById("about"); 
+  sekcjaAbout.scrollIntoView({behavior: 'smooth'});
+};
+
+document.getElementById("wina_menu1").onclick = function(){
+  const sekcjaWina = document.getElementById("wina"); 
+  sekcjaWina.scrollIntoView({behavior: 'smooth'});
+};
+
+document.getElementById("oferta_menu1").onclick = function(){
+  const sekcjaOferta = document.getElementById("oferta"); 
+  sekcjaOferta.scrollIntoView({behavior: 'smooth'});
+};
+
+document.getElementById("kontakt_menu1").onclick = function(){
+  const sekcjaKontakt = document.getElementById("kontakt"); 
+  sekcjaKontakt.scrollIntoView({behavior: 'smooth'});
+};
+
+document.getElementById("glowna_strona").onclick = function(){
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
 
 btn.addEventListener('click', ()=>{
   window.scrollTo({top: 0, behavior: 'smooth'});
